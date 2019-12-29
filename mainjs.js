6f3fc3bc2238398ce36ed7efad0f06d9fdea1b8f
jtman8 <35845155+jtman8@users.noreply.github.com>
2019-12-29 11:58:07 -0500

    Delete mainjs.js

--- mainjs.js
+++
@@ -1,9 +0,0 @@
-$(".button.loading").on('click', function(){
-    $("#image-loading").addClass("image-rotate-horizontal");
-});
-
-$(".button.stop").on('click', function(){
-    $("#image-loading").removeClass("image-rotate-horizontal");
-});
-
-$("text").lettering();
